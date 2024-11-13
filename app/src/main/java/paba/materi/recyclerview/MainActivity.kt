c62e61ba7ad5946284b609a738df0b1e700c3309
EdselGitHub <149871435+EdselGitHub@users.noreply.github.com>
2024-11-13 19:56:08 +0700

    Initial commit

---
+++ app/src/main/java/paba/materi/recyclerview/MainActivity.kt
@@ -0,0 +1,58 @@
+package paba.materi.recyclerview
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var _nama: Array<String>
+    private lateinit var _karakter: Array<String>
+    private lateinit var _deskripsi: Array<String>
+    private lateinit var _gambar: Array<String>
+    private lateinit var _rvWayang: RecyclerView
+
+    private var arWayang = arrayListOf<wayang>()
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        _rvWayang = findViewById(R.id.rvWayang)
+
+        SiapkanData()
+        TambahData()
+        TampilkanData()
+    }
+
+    private fun SiapkanData() {
+        _nama = resources.getStringArray(R.array.namaWayang)
+        _deskripsi = resources.getStringArray(R.array.deskripsiWayang)
+        _karakter = resources.getStringArray(R.array.karakterUtamaWayang)
+        _gambar = resources.getStringArray(R.array.gambarWayang)
+    }
+
+    private fun TambahData() {
+        for (position in _nama.indices) {
+            val data = wayang(
+                _gambar[position],
+                _nama[position],
+                _karakter[position],
+                _deskripsi[position]
+            )
+            arWayang.add(data)
+        }
+    }
+
+    private fun TampilkanData() {
+       _rvWayang.layoutManager = LinearLayoutManager(this)
+
+        // _rvWayang.layoutManager = GridLayoutManager(this,2) // Grid Layout
+        // _rvWayang.layoutManager = StaggeredGridLayoutManager(2,LinearLayoutManager.VERTICAL) // StaggeredG Grid Layout
+
+        _rvWayang.adapter = adapterRecView(arWayang)
+    }
+}
